565346242b6d37d9d383a84dbcee234f9b96a96b
mahak <kaushikikes@gmail.com>
2026-02-19 18:44:43 +0530

    working

--- password.js
+++ password.js
@@ -7,11 +7,12 @@ let numbers = document.getElementById("numbers");
 let symbols = document.getElementById("symbols");
 let getbtn = document.getElementById("getbtn");
 let copyIcon = document.getElementById("copyIcon");
-  SliderValue .textContent = inputSlider.ariaValueMax;
+
+  sliderValue.textContent = inputSlider.Value;
   inputSlider.addEventListener('input',()=> {
-    SliderValue.textContent = inputSlider.Value;
+    sliderValue.textContent = inputSlider.Value;
   });
-  getbtn.addEventListener('Click',()=>{
+  getbtn.addEventListener('click',()=>{
     passbox.Value = generatePassword();
   })
 
@@ -23,7 +24,29 @@ let copyIcon = document.getElementById("copyIcon");
    function generatePassword(){
     let generatePassword= " ";
     let allchars = " ";
-     
-  
-
+     allchars += lowercase,checked?lowercase:" ";
+     allchars += uppercase,checked?uppercase:" ";
+     allchars += Numbers,checked?allNubers:" ";
+     allchars += Symbols,checked?allchars:" ";
+     if(allchars == " "|| allchars.length == 0){
+      return generatePassword;
+     }
+     let i =1;
+     while(i<= inputSlider.value){
+      genPassword += allchars.charAt(Math.floor
+        (Math.random()*allchars.length));
+        i++;
+     }
+     return genPassword;
    }
+   copyIcon.addEventListener('click',()=>{
+    if(passbox.Value !=" "|| passbox.value.length >=1){
+      navigator.clipboard.writeText(passbox.value);
+      copyIcon.innerText = "Check";
+      copyIcon.title = "Password Copied";
+      setTimeout(()=>{
+        copyIcon.innerHTML = "content_copy";
+        copyIcon.title=" ";
+      },3000)
+    }
+   });
